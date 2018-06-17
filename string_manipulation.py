dd4ac97a4e61f87816b781a6e0c4e865576a007d
Gene Essel <iosdevgene@gmail.com>
2018-06-17 17:30:09 -0400

    some new string stripping and indexing

--- string_manipulation.py
+++ string_manipulation.py
@@ -2,3 +2,12 @@
 
 hello = "Gather the thieves of Alibaba"
 print(hello.replace("a", "e"))
+
+author = "Kafka"
+
+def pirn():
+    for x in range(len(author)):
+        print(author[-x])
+
+
+pirn()
