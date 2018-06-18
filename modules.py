2db3754758c3f2e02cdc707f610e278c69ec9013
Gene Essel <eugene2essel@gmail.com>
2018-06-18 07:17:41 -0400

    new imports

---
+++ modules.py
@@ -0,0 +1,13 @@
+# modules
+
+import calendar
+import math
+import random
+import statistics
+
+print(math.pow(3, 7))
+
+print(random.randint(0, 100))
+
+nums = [1, 5, 33, 12, 46, 33, 2]
+statistics.mean(nums)
