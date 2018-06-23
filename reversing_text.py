1d2c7c746b17e6b8e912b1cc9240d9a330826271
Gene Essel <eugene2essel@gmail.com>
2018-06-23 06:23:58 -0400

    added some functionality

--- reversing_text.py
+++ reversing_text.py
@@ -1,11 +1,14 @@
 #this is a test of words
 
 word = input("What word do you want to reverse: ")
-def word_submitted(words):
+def word_submitted():
     """
     Function to accept words
     param: word = str.
     """
+    
+    reversed_words = word[::-1]
+    print(reversed_words)
 
-    pass
+word_submitted()
     
