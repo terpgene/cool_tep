277c78b49c2406905f8dc65fa3122ae64f5c1909
Gene Essel <eugene2essel@gmail.com>
2018-06-23 06:19:46 -0400

    modified parameter for function

--- reversing_text.py
+++ reversing_text.py
@@ -1,7 +1,7 @@
 #this is a test of words
 
-word = input("What word do you want to reverse? ")
-def word_submitted(word):
+word = input("What word do you want to reverse: ")
+def word_submitted(words):
     """
     Function to accept words
     param: word = str.
