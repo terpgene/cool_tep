fce7f90c1fd20cb6e41a5a04c4043db9ad266536
Gene Essel <eugene2essel@gmail.com>
2018-06-25 22:54:45 -0400

    edits

--- reversing_text.py
+++ reversing_text.py
@@ -1,8 +1,6 @@
 WORD = input("What word do you want to reverse: ")
 def word_submitted():
-    """
-    Function to accept words.
-    """
+    """ Function to accep."""
     
     reversed_words = WORD[::-1]
     print(reversed_words)
