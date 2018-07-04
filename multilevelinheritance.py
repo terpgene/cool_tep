a47de9c4512d8e1bc5bd1052091474a87a9f9895
Gene Essel <eugene2essel@gmail.com>
2018-07-04 08:01:08 -0400

    Multi level inheritance

---
+++ multilevelinheritance.py
@@ -0,0 +1,12 @@
+class MusicalInstruments:
+    numberOfMajorKeys = 12
+
+class StringInstruments(MusicalInstruments):
+    typeOfWood = "Tonewood"
+
+class Guitar(StringInstruments):
+    def __init__(self):
+        self.numberOfStrings = 6
+        print("This guitar consists of {} strings. It is made of {} and it can play {} keys".format(self.numberOfStrings, self.typeOfWood, self.numberOfMajorKeys))
+
+guitar = Guitar()    
